fd6df03dac66c9a35704fa86d32722488ec39e41
Kevin Corina <batchman66@aol.com>
2013-10-19 22:19:50 -0400

    Create rentals.php

---
+++ rentals.php
@@ -0,0 +1,721 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="keywords" content="Weekapaug rentals, Weekapaug summer rentals, Weekapaug vacation rentals">
+<meta name="description" content="Weekapaug RI Rentals - Great Summer Rentals in the Weekapaug RI area">
+<title>Weekapaug Rhode Island Rentals | Weekapaug Life</title>
+<style type="text/css">
+<!--
+#weekrentmap {
+	height: 800px;
+	position: relative;
+	width: 600px;
+	background-image: url(images/weekapaugmap.jpg);
+	background-repeat: no-repeat;
+	margin-left: 20px;
+	float: left;
+}
+#weekrentmap ul {
+	list-style: none;
+}
+#weekrentmap li{
+	position: absolute;
+	z-index: 20;
+	display: inline;
+	text-decoration: none;
+}
+#weekrentmap li a {
+	text-decoration: none;
+}
+#weekrentmap li a:hover {
+	text-decoration: none;
+}
+#weekrentmap #title {
+	position: absolute;
+	top: 0px;
+	text-align: center;
+	left: 610px;
+	width: 200px;
+}
+#weekrentmap #title h1 {
+	font-size: 24px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+	text-align: center;
+}
+#weekrentmap #title h2 {
+	font-family: Georgia, "Times New Roman", Times, serif;
+	font-size: 14px;
+}
+#legend {
+	list-style-type: none;
+	height: 124px;
+	width: 160px;
+	margin-top: 10px;
+	margin-left: 110px;
+	border: thin solid #993;
+	background-image: url(images/legend.gif);
+	position: relative;
+	background-repeat: no-repeat;
+}
+
+#legend #beds2 {
+	left: 10px;
+	top: 2px;
+	height: 14px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	font-variant: small-caps;
+	padding-left: 5px;
+	padding-top: 2px;
+	font-size: 13px;
+	font-weight: 700;
+	position: absolute;
+}
+#legend #beds3 {
+	left: 10px;
+	top: 22px;
+	height: 14px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	padding-top: 2px;
+	padding-left: 5px;
+	font-weight: 700;
+	font-size: 13px;
+	font-variant: small-caps;
+	position: absolute;
+}
+#legend #beds4 {
+	left: 10px;
+	top: 42px;
+	height: 14px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	padding-top: 2px;
+	padding-left: 5px;
+	font-size: 13px;
+	font-weight: 700;
+	font-variant: small-caps;
+	position: absolute;
+}
+#legend #beds5 {
+	left: 10px;
+	top: 62px;
+	height: 14px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	padding-top: 2px;
+	padding-left: 5px;
+	font-size: 13px;
+	font-weight: 700;
+	font-variant: small-caps;
+	position: absolute;
+}
+#legend #beds6 {
+	left: 10px;
+	top: 82px;
+	height: 16px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 13px;
+	font-weight: 700;
+	font-variant: small-caps;
+	padding-top: 2px;
+	padding-left: 5px;
+	position: absolute;
+}
+#legend #rented {
+	left: 10px;
+	top: 102px;
+	height: 14px;
+	width: 200px;
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+	font-variant: small-caps;
+	padding-left: 5px;
+	padding-top: 2px;
+	font-size: 13px;
+	font-weight: 700;
+	position: absolute;
+}
+#ChapmanRoad7 {
+	left: 59px;
+	top: 296px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad8 {
+	left: 50px;
+	top: 265px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad9 {
+	left: 63px;
+	top: 282px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad12 {
+	left: 76px;
+	top: 256px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad25 {
+	left: 158px;
+	top: 266px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad45 {
+	left: 280px;
+	top: 256px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad60 {
+	left: 490px;
+	top: 240px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#ChapmanRoad64 {
+	left: 515px;
+	top: 300px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad5 {
+	left: 215px;
+	top: 485px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad11 {
+	left: 210px;
+	top: 450px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad13 {
+	left: 195px;
+	top: 460px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad39 {
+	left: 140px;
+	top: 350px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad44 {
+	left: 100px;
+	top: 312px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad50 {
+	left: 92px;
+	top: 280px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad52 {
+	left: 82px;
+	top: 270px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad56 {
+	left: 75px;
+	top: 235px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad57 {
+	left: 105px;
+	top: 225px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#FenwayRoad58 {
+	left: 75px;
+	top: 220px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue6 {
+	left: 75px;
+	top: 365px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue10 {
+	left: 95px;
+	top: 375px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue13 {
+	left: 120px;
+	top: 365px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue14 {
+	left: 130px;
+	top: 390px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue17 {
+	left: 162px;
+	top: 365px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue19 {
+	left: 175px;
+	top: 365px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue20 {
+	left: 180px;
+	top: 390px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue26 {
+	left: 230px;
+	top: 390px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue42 {
+	left: 360px;
+	top: 380px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue43 {
+	left: 330px;
+	top: 355px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#KnowlesAvenue46 {
+	left: 385px;
+	top: 380px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue7 {
+	left: 370px;
+	top: 110px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue14 {
+	left: 410px;
+	top: 145px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue18 {
+	left: 410px;
+	top: 160px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue54 {
+	left: 430px;
+	top: 435px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue65 {
+	left: 393px;
+	top: 510px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue66 {
+	left: 433px;
+	top: 510px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue74 {
+	left: 420px;
+	top: 570px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#MeadowAvenue75 {
+	left: 400px;
+	top: 570px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NatchaugRoad2 {
+	left: 275px;
+	top: 230px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NoyesNeckRoad38 {
+	left: 330px;
+	top: 100px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NoyesNeckRoad42 {
+	left: 350px;
+	top: 125px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NoyesNeckRoad43 {
+	left: 310px;
+	top: 135px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NoyesNeckRoad90 {
+	left: 370px;
+	top: 430px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#NoyesNeckRoad112 {
+	left: 360px;
+	top: 575px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#PasspataugAvenue8 {
+	left: 225px;
+	top: 330px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#PasspataugAvenue23 {
+	left: 220px;
+	top: 440px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#SprayRockRoad32 {
+	left: 460px;
+	top: 475px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#SprayRockRoad17B {
+	left: 460px;
+	top: 620px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#SprayRockRoad19 {
+	left: 480px;
+	top: 610px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#UplandRoad2 {
+	left: 130px;
+	top: 307px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#UplandRoad6 {
+	left: 160px;
+	top: 305px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WawaloamDrive14 {
+	left: 93px;
+	top: 410px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WawaloamDrive16 {
+	left: 108px;
+	top: 420px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WawaloamDrive36 {
+	left: 230px;
+	top: 513px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WaxcadowaAvenue10 {
+	left: 300px;
+	top: 460px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WeekapaugRoad11 {
+	left: 33px;
+	top: 290px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WilliamsAvenue6 {
+	left: 375px;
+	top: 50px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WilliamsAvenue12 {
+	left: 420px;
+	top: 50px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WilliamsAvenue14 {
+	left: 440px;
+	top: 50px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WilliamsAvenue25 {
+	left: 495px;
+	top: 120px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WilliamsAvenue30 {
+	left: 510px;
+	top: 90px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WinonaAvenue3 {
+	left: 250px;
+	top: 485px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+#WinonaAvenue11 {
+	left: 290px;
+	top: 500px;
+	height: 14px;
+	width: 14px;
+	text-decoration: none;
+}
+.popup
+{
+	position:absolute;
+	top:-20px;
+	width:200px;
+	background-color:#039;
+	padding:5px;
+	color:#FFFFFF;
+	font-family:Georgia, "Times New Roman", Times, serif;
+	font-weight:normal;
+	font-size:12pt;
+	z-index:1000;
+	visibility:hidden;
+	border: 4px ridge #09C;
+	text-align: center;
+}
+
+-->
+</style>
+<link href="wl.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<?php
+   include("weeklibrary/login.php");
+   login();
+?>
+<script language="JavaScript"> 
+function ShowPop(id)
+{
+   document.getElementById(id).style.visibility = "visible";
+}
+function HidePop(id)
+{
+   document.getElementById(id).style.visibility = "hidden";
+}
+</script>
+<script type="text/javascript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<body>
+<div id="container">
+<div id="header">
+    <img src="images/weekapauglife.jpg" title="Weekapaug Rhode Island Real Estate and Summer Rentals" />
+</div>
+<div id="navbar">
+   <ul>
+      <li><div align="center"><a href="rentallist.php"><img src="images/lists.jpg" border="0" title="Weekapaug Rental List" /><br />
+      Weekapaug Rental List</a></div></li>
+      <p>&nbsp;</p>
+      <li><div align="center"><a href="http://watchhilllife.com/index2.php"><img src="images/WHHome.gif" border="0" title="Watch Hill Life" /><br />
+      We also have Watch Hill Rentals at our sister site WatchHillLife.com</a></div>   </li>
+   </ul><br />  <?php include("nav.php"); ?>
+</div>
+     <div id="weekrentmap"> 
+      <div id="title"><h1>Weekapaug, Rhode Island Rentals</h1><h2>Click Rental List at left for Haversham and Other Rentals</h2></div>
+      <ul id="legend">
+        <li id="beds2"><img src="images/bedrooms2.gif" border="0"> 1-2 Bedrooms</li>
+        <li id="beds3"><img src="images/bedrooms3.gif" border="0"> 3 Bedrooms</li>
+        <li id="beds4"><img src="images/bedrooms4.gif" border="0"> 4 Bedrooms</li>
+        <li id="beds5"><img src="images/bedrooms5.gif" border="0"> 5 Bedrooms</li>
+        <li id="beds6"><img src="images/bedrooms6.gif" border="0"> 6+ Bedrooms</li>
+        <li id="rented"><img src="images/rented.gif" border="0"> Rented</li>
+      </ul>
+
+<?php     
+     $i = 1;
+     $query_rsRentals = "SELECT * FROM summerrentals WHERE town = 'Weekapaug' ORDER BY house ASC";
+     $rsRentals = mysql_query($query_rsRentals) or die(mysql_error());
+     while($row = mysql_fetch_array($rsRentals)) {
+	    if ($row['summer'] != "") { 
+			$rentalid = $row['id'];
+			if ($row['beds'] == '1' || $row['beds'] == '2')
+			   $beds = 2;
+			else if  ($row['beds'] == '3' || $row['beds'] == '4' || $row['beds'] == '5')
+			   $beds = $row['beds'];
+			else
+			   $beds = 6;
+			$strnumber = $row['strnumber'];
+			$streetnumber = str_replace(" ","",$strnumber);
+			$address = $row['street'];
+			$street = str_replace(" ","",$address);
+			$summer = $row['summer'];
+			$x = $row['x'];
+			$y = $row['y'];
+			$left = 600 - $x;
+?>			
+      <ul>
+        <li id="<?php echo $street; ?><?php echo $streetnumber; ?>">
+        <span id="term<?php echo $i; ?>" class="popup" style="left:<?php echo $left; ?>px">Weekapaug, RI<br />Summer Rental<img src="images/rentals/<?php echo $street; ?><?php echo $streetnumber; ?>.jpg"  /><br /><?php echo $streetnumber; ?> <?php echo $address; ?><br /><br />2014 Availability: <?php echo $summer; ?><br /><br />Click on button for more pictures and info</span>        
+        <a href="rentals2.php?rid=<?php echo $rentalid; ?>" onclick="HidePop('term<?php echo $i; ?>');" onmouseover="ShowPop('term<?php echo $i; ?>');" onmouseout="HidePop('term<?php echo $i; ?>');"><img src="images/bedrooms<?php echo $beds; ?>.gif" border="0" style="z-index:10"></a>
+        </li>
+      </ul>
+<?php 
+// title="<?php echo $strnumber." ".$address;  - Available: <?php echo $row['summer'];       
+
+//		   $event_title .= "<span id=\"term".$i."\" class=\"popup\">".$classdesc."</span>";
+//		   $event_title .= "<a href=\"javascript:void(0);\" onMouseover=\"ShowPop('term".$i."');\" onMouseout=\"HidePop('term".$i."');\">";
+//		   $event_title .= $etcal."</a><br/>";
+//           echo "<h3>$event_title</h3>\n";
+
+         $i++;
+		}
+	 else {
+		  if ($row['deleted'] == 0) {
+ 			$rentalid = $row['id'];
+			$strnumber = $row['strnumber'];
+			$streetnumber = str_replace(" ","",$strnumber);
+			$address = $row['street'];
+			$street = str_replace(" ","",$address);
+			$summer = $row['summer'];
+			$x = $row['x'];
+			$y = $row['y'];
+			$left = 600 - $x;
+?>
+      <ul>
+        <li id="<?php echo $street; ?><?php echo $streetnumber; ?>">
+        <span id="term<?php echo $i; ?>" class="popup" style="left:<?php echo $left; ?>px"><img src="images/rentals/<?php echo $street; ?><?php echo $streetnumber; ?>.jpg"  /><br /><?php echo $streetnumber; ?> <?php echo $address; ?><br /><br />2014 Availability: RENTED<br /><br />Click on button for more pictures and info</span>        
+        <a href="rentals2.php?rid=<?php echo $rentalid; ?>" onclick="HidePop('term<?php echo $i; ?>');" onmouseover="ShowPop('term<?php echo $i; ?>');" onmouseout="HidePop('term<?php echo $i; ?>');"><img src="images/rented.gif" border="0" style="z-index:10"></a>
+        </li>
+      </ul>
+<?php
+         $i++;
+	    }
+	 }
+     }  
+?>
+  </div>
+
+<div id="footer">
+  <?php include("footer.php"); ?>
+</div> 
+</div>           
+</body>
+
+
+</html>
